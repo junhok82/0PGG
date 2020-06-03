407d2e949d7a40ff8dcf49e284525f2edba5d303
junhok82 <junhok82@gmail.com>
2020-06-03 21:25:39 +0900

    preparation : 소환사 숙련도 정보 파싱하기
    1. DELETE
       -
    
    2. CREATE
       - ChampionMastery.java : 소환사 숙련도 정보 DTO
       - ChampionMasteryParser.java : json 형식으로 소환사 숙련도 정보를 얻는 파서
    
    3.UPDATE
       - Controller.java :
          - 소환사 정보를 얻어온 encrypted id를 통해서 숙련도 정보를 조회

--- SpringGG/src/main/java/com/junho/dto/Summoner.java
+++ SpringGG/src/main/java/com/junho/dto/Summoner.java
@@ -2,6 +2,7 @@ package com.junho.dto;
 
 public class Summoner {
 
+	/** Summoner : 소환사 정보 **/
 	// Encrypted account ID
 	private String accountId;
 	
@@ -24,8 +25,7 @@ public class Summoner {
 	
 	// Summoner level associated with the summoner.
 	private long summonerLevel;
-	
-	
+		
 	public Summoner() {}
 	public Summoner(String accountId, int profileIconId, long revisionDate, String name, String id, String puuid,
 			long summonerLevel) {
@@ -37,61 +37,48 @@ public class Summoner {
 		this.puuid = puuid;
 		this.summonerLevel = summonerLevel;
 	}
-
 	public String getAccountId() {
 		return accountId;
 	}
-
 	public void setAccountId(String accountId) {
 		this.accountId = accountId;
 	}
-
 	public int getProfileIconId() {
 		return profileIconId;
 	}
-
 	public void setProfileIconId(int profileIconId) {
 		this.profileIconId = profileIconId;
 	}
-
 	public long getRevisionDate() {
 		return revisionDate;
 	}
-
 	public void setRevisionDate(long revisionDate) {
 		this.revisionDate = revisionDate;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getPuuid() {
 		return puuid;
 	}
-
 	public void setPuuid(String puuid) {
 		this.puuid = puuid;
 	}
-
 	public long getSummonerLevel() {
 		return summonerLevel;
 	}
-
 	public void setSummonerLevel(long summonerLevel) {
 		this.summonerLevel = summonerLevel;
 	}
+
 	
 }
